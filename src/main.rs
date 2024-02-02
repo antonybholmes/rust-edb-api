77d76f02f4d5927323b5c65ed97bc201230b8c35
Antony Holmes <antony@antonyholmes.dev>
2024-02-02 17:58:47 -0500

    fix: Added Rocket

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use rocket::serde::{Serialize, json::Json};
  
+ 
 mod tests;
 
 #[derive(Serialize)]
@@ -10,12 +11,33 @@ pub struct Message {
     pub message: String,
 }
 
+#[derive(Serialize)]
+pub struct DNA {
+    pub location: String,
+    pub dna: String
+}
+
 #[get("/")]
 fn index() -> Json<Message> {
     Json(Message{message:"cake".to_string()})
 }
 
+#[get("/dna")]
+fn dna_route() -> Json<DNA>{
+    let loc = dna::Location::parse("chr1:100000-100100");
+
+    println!("{}", loc);
+
+    let dir:&str="/ifs/scratch/cancer/Lab_RDF/ngs/dna/hg19";
+
+    let dna_db : dna::DNA = dna::DNA::new(dir);
+
+    let dna = dna_db.get_dna(&loc, true, true);
+
+    return Json(DNA{location:loc.to_string(), dna})
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index]).mount("/v1", routes![dna_route])
 }
