fc02d77e434026bfe0ee741841dcf3c6f669d5d3
Antony Holmes <antony@antonybholmes.com>
2024-02-03 23:11:02 -0500

    fix: Improved errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use loctogene::{FeatureRecord, Features};
 use rocket::{
     response::status::BadRequest,
     serde::{json::Json, Serialize},
@@ -48,12 +49,43 @@ fn dna_route() -> Result<Json<DNAJsonResp>, BadRequest<Json<MessageResp>>> {
     }));
 }
 
-#[get("/within")]
-fn within_genes_route() -> Result<Json<loctogene::Features>, BadRequest<Json<MessageResp>>>{
-    let loc: dna::Location = match dna::Location::parse("chr3:187721377-187745725") {
+#[get("/test?<chr>&<p>")]
+fn plop(chr: &str, p: u32) -> Result<String, BadRequest<String>> {
+    let loc: dna::Location = match dna::Location::new(chr, 1, 2) {
+        Ok(loc) => loc,
+        Err(err) => return Err(BadRequest(err)),
+    };
+
+    Ok(format!("{} {}", loc, p))
+}
+
+#[get("/within?<chr>&<start>&<end>")]
+fn within_genes_route(
+    chr: Option<&str>,
+    start: Option<u32>,
+    end: Option<u32>,
+) -> Result<Json<loctogene::Features>, BadRequest<Json<MessageResp>>> {
+    let c = match chr {
+        Some(c) => c,
+        None => "chr3",
+    };
+
+    let s = match start {
+        Some(s) => s,
+        None => 187721377,
+    };
+
+    let e = match end {
+        Some(e) => e,
+        None => 187721377,
+    };
+
+    let loc: dna::Location = match dna::Location::new(c, s, e) {
         Ok(loc) => loc,
         Err(err) => return Err(BadRequest(Json(MessageResp { message: err }))),
     };
+
+    print!("{}", loc.to_string());
 
     let genesdb: loctogene::Loctogene = match loctogene::Loctogene::new(
         "/home/antony/development/go/docker-go-edb-api/data/loctogene/grch38.db",
@@ -62,12 +94,13 @@ fn within_genes_route() -> Result<Json<loctogene::Features>, BadRequest<Json<Mes
         Err(err) => return Err(BadRequest(Json(MessageResp { message: err }))),
     };
 
-    let records: loctogene::Features = match genesdb.get_genes_within(&loc, 1) {
-        Ok(records) => records,
-        Err(err) => return Err(BadRequest(Json(MessageResp { message: err }))),
-    };
+    // let records: loctogene::Features = match genesdb.get_genes_within(&loc, 1) {
+    //     Ok(records) => records,
+    //     Err(err) => return Err(BadRequest(Json(MessageResp { message: err }))),
+    // };
 
-    return Ok(Json(records));
+    //Ok(Json(Features{ location: "".to_string(), level: "".to_string(), features: Vec::<FeatureRecord>::new() }))
+    Err(BadRequest(Json(MessageResp { message: "ckk".to_string() })))
 }
 
 #[get("/closest")]
@@ -99,6 +132,6 @@ fn rocket() -> _ {
         .mount("/v1/dna", routes![dna_route])
         .mount(
             "/v1/genes",
-            routes![within_genes_route, closest_genes_route],
+            routes![within_genes_route, closest_genes_route, plop],
         )
 }
