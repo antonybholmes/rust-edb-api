b9dc0a427bb35bf7fb9bfe1bb9b066a864639592
Antony Holmes <antony@antonyholmes.dev>
2024-02-15 09:31:07 -0500

    fix: Bug

--- test_urls.sh
+++ test_urls.sh
@@ -11,7 +11,7 @@ curl -v -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.e
 curl -v -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMDEwMjZGMTktMEUxQS00OEY0LTk4RjgtRUMyOUEwNTlBMjRGIiwiZW1haWwiOiJhbnRvbnlAYW50b255aG9sbWVzLmRldiIsImV4cCI6MTcwODAxODI0MH0.gGE8FyO4jtcXxybGcST07MhGf4LMsGUC-HGlkNgzBFGL5-Y1A2PPhBFsAZ5tcExrm05KIBdx-DxK1RFkdxBtt" -H "Content-Type: application/json" -d '{"locations":[{"chr":"chr1","start":100000,"end":100100}]}' "localhost:8080/dna/grch38?n=5"
 
 
-curl -v -X POST -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAxMDI2RjE5LTBFMUEtNDhGNC05OEY4LUVDMjlBMDU5QTI0RiIsImVtYWlsIjoiYW50b255QGFudG9ueWhvbG1lcy5kZXYiLCJleHAiOjE3MDgwMzk0Mzd9.mW-XdwcmhBYEK4hwRiW6Fn7Zonw0_0nA7atlMEbRTSw" -H "Content-Type: application/json" -d '{"locations":[{"chr":"chr10","start":1043441,"end":1044114}]}' "localhost:8080/genes/within/grch38"
+curl -v -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjoiMDEwMjZGMTktMEUxQS00OEY0LTk4RjgtRUMyOUEwNTlBMjRGIiwiZW1haWwiOiJhbnRvbnlAYW50b255aG9sbWVzLmRldiIsImV4cCI6MTcwODAzOTk1N30.1rL6ZgisnZUebKBU-qyp0kOy7o_u_6LoNXgHGAndegVYTMzoS9tZ-F3LA_ZcU44lLFn_6fSM5cozHE1hNYXwpg" -H "Content-Type: application/json" -d '{"locations":[{"chr":"chr10","start":1043441,"end":1044114}]}' "localhost:8080/genes/within/grch38"
 
 
 
